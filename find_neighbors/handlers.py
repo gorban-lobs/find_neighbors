92d1166855ca11301ada1655d6a22c5679bac19c
Gorban Lobs <lobs.gorban@yandex.ru>
2019-08-28 20:08:07 +0300

    Add simple add_user handler

--- find_neighbors/handlers.py
+++ find_neighbors/handlers.py
@@ -5,6 +5,16 @@ async def show(request):
     return web.Response(text='12345')
 
 
+async def add_user(request):
+    json_data = await request.json()
+    new_id = len(request.app.users) + 1
+    request.app.users[str(new_id)] = {
+        'name': json_data['name']
+    }
+    return web.Response(text='Added')
+
+
 async def get_neighbors(request):
     params = request.rel_url.query
-    return web.Response(text=str(params['name']))
+    cur_user = request.app.users.get(params['id'], 'No such user')
+    return web.Response(text=str(cur_user))
--- find_neighbors/routes.py
+++ find_neighbors/routes.py
@@ -1,6 +1,7 @@
-from handlers import show, get_neighbors
+from handlers import show, get_neighbors, add_user
 
 
 def setup_routes(app):
     app.router.add_get('/', show)
     app.router.add_get('/lookup', get_neighbors)
+    app.router.add_post('/users', add_user)
