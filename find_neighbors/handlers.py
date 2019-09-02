e743a61fbf8bb51c1d8ec0bd2e7ca40802451f03
Gorban Lobs <lobs.gorban@yandex.ru>
2019-09-02 15:55:07 +0300

    Add checking request parameters

--- find_neighbors/handlers.py
+++ find_neighbors/handlers.py
@@ -1,29 +1,48 @@
+import json
 from aiohttp import web
 
 from db_logic import add_user_to_db, get_neighbors_by_ids
 
 
 async def add_user(request):
-    json_data = await request.json()
-    user_id = await add_user_to_db(request.app['conn'],
-                                   json_data['name'],
-                                   float(json_data['lon']),
-                                   float(json_data['lat']))
-    request.app['index'].add_items([[float(json_data['lon']),
-                                     float(json_data['lat'])]],
-                                   user_id)
+    try:
+        json_data = await request.json()
+        lon = float(json_data['lon'])
+        lat = float(json_data['lat'])
+        name = str(json_data['name'])
+    except json.decoder.JSONDecodeError:
+        return web.Response(text='No json data')
+    except KeyError:
+        return web.Response(text='Wrong number of parameters')
+    except ValueError:
+        return web.Response(text='Wrong parameter type')
+
+    user_id = await add_user_to_db(request.app['conn'], name, lon, lat)
+    request.app['index'].add_items([[lon, lat]], user_id)
     return web.Response(text='Added')
 
 
 async def get_neighbors(request):
-    if len(request.app['index'].get_ids_list()) == 0:
+    index_size = len(request.app['index'].get_ids_list())
+    if index_size == 0:
         return web.Response(text='There is no users in database')
 
     params = request.rel_url.query
-    lon = float(params['lon'])
-    lat = float(params['lon'])
-    radius = int(params['radius'])
-    limit = int(params['limit'])
+    try:
+        lon = float(params['lon'])
+        lat = float(params['lat'])
+        radius = int(params['radius'])
+        limit = int(params['limit'])
+    except KeyError:
+        return web.Response(text='Wrong number of parameters')
+    except ValueError:
+        return web.Response(text='Wrong parameter type')
+
+    if not (-180 <= lon <= 180 and -90 <= lat <= 90):
+        return web.Response(text='Wrong coordinates')
+
+    if limit > index_size:
+        return web.Response(text=f'Limit must be <= {index_size}')
 
     ids = request.app['index'].search_neighbors(lon, lat, radius, limit)
     if ids.size == 0:
